982ee05eeb339bd79a335a67706942a63b02db53
Joel Azemar <joel.azemar@gmail.com>
2021-06-15 17:23:44 +0200

    Add Post Resources

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem "byebug", platforms: %i[mri mingw x64_mingw]
   gem "factory_bot_rails"
+  gem "ffaker"
   gem "rubocop-minitest", require: false
   gem "rubocop-performance", require: false
   gem "rubocop-rails", require: false
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,14 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # http://chromedriver.chromium.org/capabilities
+
+  driver = ENV["CHROME_HEADLESS"] ? :headless_chrome : :chrome
+
+  driven_by :selenium, using: driver do |driver_options|
+    driver_options.add_argument("--no-sandbox")
+    driver_options.add_argument("--disable-dev-shm-usage")
+    driver_options.add_argument("--disable-gpu")
+    driver_options.add_argument("--window-size=1400,1400")
+  end
 end

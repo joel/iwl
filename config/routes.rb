f26ffc071a43a3a74a498566227838b1b4bd2047
Joel Azemar <joel.azemar@gmail.com>
2021-06-22 15:10:47 +0200

    Add Welcome Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  root :to => "welcome#index"
+
+  get 'welcome/index'
+
   concern :imageable do
     resources :images
   end

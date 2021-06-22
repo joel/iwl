d0def47bcf7984848737a39c84c30bc87052d7d3
Joel Azemar <joel.azemar@gmail.com>
2021-06-22 16:34:34 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  root :to => "welcome#index"
+  root to: "welcome#index"
 
-  get 'welcome/index'
+  get "welcome/index"
 
   concern :imageable do
     resources :images
